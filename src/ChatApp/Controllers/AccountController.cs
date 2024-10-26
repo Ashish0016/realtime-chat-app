b6d05dc9b50946102b20d9817981f1fc700ebe3a
Ashish Naikwadi <ashish.naikwadi@fusionstak.com>
2024-10-26 20:35:51 +0530

    staging - Added business logic layer. - Added controller

---
+++ src/ChatApp/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Core.Feature.AccountFeature.UserLogin;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public AccountController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(
+            [FromBody] UserLoginModel model)
+        {
+            return Ok(await _mediator.Send(model));
+        }
+    }
+}
---
+++ src/Core/Feature/AccountFeature/UserLogin/UserLoginHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+
+namespace Core.Feature.AccountFeature.UserLogin
+{
+    public record UserLoginModel(string Email, string Password) : IRequest<UserLoginResult>;
+    public record UserLoginResult(int Id, string UserName);
+    public class UserLoginHandler : IRequestHandler<UserLoginModel, UserLoginResult>
+    {
+        public Task<UserLoginResult> Handle(UserLoginModel request, CancellationToken cancellationToken)
+        {
+            if (string.IsNullOrWhiteSpace(request.Email))
+            {
+                return Task.FromResult(new UserLoginResult(0, string.Empty));
+            }
+
+            Random random = new Random();
+            int randomNumber = random.Next(1, 10);
+
+            string[] userName = request.Email.Split("@");
+
+            UserLoginResult result = new UserLoginResult(randomNumber, userName[0]);
+
+            return Task.FromResult(result);
+        }
+    }
+}
