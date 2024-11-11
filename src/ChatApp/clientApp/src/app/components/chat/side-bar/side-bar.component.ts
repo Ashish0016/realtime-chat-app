2d096c7dcd877bed97ce5c50ec42607b1b04c15d
Ashish Naikwadi <ashish.naikwadi@fusionstak.com>
2024-11-11 22:49:02 +0530

    staging - Created the main page layout.

--- src/ChatApp/clientApp/src/app/components/chat/side-bar/side-bar.component.ts
+++ src/ChatApp/clientApp/src/app/components/chat/side-bar/side-bar.component.ts
@@ -7,6 +7,94 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideBarComponent implements OnInit {
 
+  public users = [
+    {
+      'avatar': 'fas fa-user text-blue-500 text-2xl',
+      'name':'Ashish Naikwadi',
+      'lastMessage':"Are you there?"
+    },
+    {
+      'avatar': 'fas fa-user text-blue-500 text-2xl',
+      'name':'Dipak Pawale',
+      'lastMessage':"hello!"
+    },
+    {
+      'avatar': 'fas fa-user text-blue-500 text-2xl',
+      'name':'Shubham Gorde',
+      'lastMessage':"hello!"
+    },
+    {
+      'avatar': 'fas fa-user text-blue-500 text-2xl',
+      'name':'Suraj Naikwadi',
+      'lastMessage':"hello!"
+    },
+    {
+      'avatar': 'fas fa-user text-blue-500 text-2xl',
+      'name':'Joe Biden',
+      'lastMessage':"hello!"
+    },
+    {
+      'avatar': 'fas fa-user text-blue-500 text-2xl',
+      'name':'Mangesh Nehe',
+      'lastMessage':"hello!"
+    },
+    {
+      'avatar': 'fas fa-user text-blue-500 text-2xl',
+      'name':'Pratiksha Gunjal',
+      'lastMessage':"hello!"
+    },
+    {
+      'avatar': 'fas fa-user text-blue-500 text-2xl',
+      'name':'Suvarna Jadhav',
+      'lastMessage':"hello!"
+    },
+    // {
+    //   'avatar': 'fas fa-user text-blue-500 text-2xl',
+    //   'name':'Dipak Pawale',
+    //   'lastMessage':"hello!"
+    // },
+    // {
+    //   'avatar': 'fas fa-user text-blue-500 text-2xl',
+    //   'name':'Dipak Pawale',
+    //   'lastMessage':"hello!"
+    // },
+    // {
+    //   'avatar': 'fas fa-user text-blue-500 text-2xl',
+    //   'name':'Ashish Naikwadi',
+    //   'lastMessage':"Are you there?"
+    // },
+    // {
+    //   'avatar': 'fas fa-user text-blue-500 text-2xl',
+    //   'name':'Ashish Naikwadi',
+    //   'lastMessage':"Are you there?"
+    // },
+    // {
+    //   'avatar': 'fas fa-user text-blue-500 text-2xl',
+    //   'name':'Ashish Naikwadi',
+    //   'lastMessage':"Are you there?"
+    // },
+    // {
+    //   'avatar': 'fas fa-user text-blue-500 text-2xl',
+    //   'name':'Ashish Naikwadi',
+    //   'lastMessage':"Are you there?"
+    // },
+    // {
+    //   'avatar': 'fas fa-user text-blue-500 text-2xl',
+    //   'name':'Ashish Naikwadi',
+    //   'lastMessage':"Are you there?"
+    // },
+    // {
+    //   'avatar': 'fas fa-user text-blue-500 text-2xl',
+    //   'name':'Ashish Naikwadi',
+    //   'lastMessage':"Are you there?"
+    // },
+    // {
+    //   'avatar': 'fas fa-user text-blue-500 text-2xl',
+    //   'name':'Ashish Naikwadi',
+    //   'lastMessage':"Are you there?"
+    // },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
